caac40008cff430344ea3fa41786d4358b8848c4
popey2700 <alex.pope@madetech.com>
2024-07-16 17:00:54 +0100

    Implement govuk-react
    Installed with node govuk-react componenets and removed
    bootstrap pages and replaced them with a very simple GDS
    focused page

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as GovUK from "govuk-react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+      <GovUK.GlobalStyle />
+      <GovUK.TopNav></GovUK.TopNav>
+      <GovUK.Page.WidthContainer>
+        <GovUK.Page.Main>
+          <GovUK.H2>Welcome to the dhsc-alpha-frontend application.</GovUK.H2>
+        </GovUK.Page.Main>
+      </GovUK.Page.WidthContainer>
+      <GovUK.Footer />
+    </>
   </React.StrictMode>
 );
 
