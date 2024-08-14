8c318b56f2f950233d04629bd44c5a9cba4aa551
popey2700 <alex.pope@madetech.com>
2024-08-14 16:27:51 +0100

    Write basic Cypress test

--- app/src/components/navbar/Navbar.test.tsx
+++ app/src/components/navbar/Navbar.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Navbar from './Navbar';
 import { MemoryRouter } from 'react-router-dom';
 
