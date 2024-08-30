956d951aba03246e6280b2c59f7fd092db8a46ca
popey2700 <alex.pope@madetech.com>
2024-08-30 16:54:14 +0100

    Implement GDS

--- vite-app/src/pages/register-page/RegisterPage.tsx
+++ vite-app/src/pages/register-page/RegisterPage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Layout from "../../components/layout/Layout";
 const RegisterPage: React.FC = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <Layout>
+        <h1>Hello world</h1>
+      </Layout>
+    </>
+  );
 };
 
 export default RegisterPage;
