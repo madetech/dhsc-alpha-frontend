c16b149e4709621969668fb3a2daf2b8d300da7d
popey2700 <alex.pope@madetech.com>
2024-09-02 16:02:53 +0100

    Create fallback loading

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import "./globals.scss";
+import LoadingPage from "./pages/loading-page/LoadingPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} fallbackElement={<LoadingPage />} />
   </StrictMode>
 );
