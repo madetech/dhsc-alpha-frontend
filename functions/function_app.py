6b19d8ee88fce7ff265a1fa856b1d415411625d5
popey2700 <alex.pope@madetech.com>
2024-09-09 16:07:39 +0100

    Remove local ManageIdentity

--- functions/function_app.py
+++ functions/function_app.py
@@ -1,6 +1,6 @@
 import azure.functions as func
 import logging
-from azure.identity import ManagedIdentityCredential, DefaultAzureCredential
+from azure.identity import ManagedIdentityCredential
 import pyodbc
 import struct
 import pandas as pd
@@ -14,7 +14,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 connection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:dapalpha-sql-data-" + os.getenv(
     "ENVIRONMENT") + ".database.windows.net,1433;Database=Analytical_Datastore;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30"
 
-credential = DefaultAzureCredential() if os.environ["ENV"] == "local" else ManagedIdentityCredential(
+credential = ManagedIdentityCredential(
     client_id=os.getenv("MANAGED_IDENTITY_CLIENT_ID"))
 
 token_bytes = credential.get_token(
@@ -22,10 +22,11 @@ token_bytes = credential.get_token(
 
 token_struct = struct.pack(
     f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
+
 SQL_COPT_SS_ACCESS_TOKEN = 1256
 
 conn = pyodbc.connect(connection_string, attrs_before={
-    SQL_COPT_SS_ACCESS_TOKEN: token_struct})
+                      SQL_COPT_SS_ACCESS_TOKEN: token_struct})
 
 
 @app.route(route="get_ascof_data")
